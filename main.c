11822b68c3ee443054abc7720382da8e6ed8a30c
Abel Salgado Romero <abelromero@gmail.com>
2020-04-12 19:05:56 +0200

    added command arguments parsing

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <X11/Xlib.h>
 #include <X11/extensions/Xinerama.h>
 
@@ -9,20 +10,48 @@ Display *display = NULL;
 XineramaScreenInfo *screensInfo = NULL;
 int screensCount;
 
+void openScreens();
+void closeScreens();
+
 XineramaScreenInfo *inintScreensInfo();
 void printScreensInfo();
 
 int main(int argc, char **argv)
 {
 
-    display = XOpenDisplay(NULL);
-    screensInfo = inintScreensInfo();
+    if (argc == 2)
+    {
+        for (int i = 1; i < argc; i++)
+        {
+            if (strcmp(argv[i], "-info") == 0)
+            {
+                openScreens();
+                printScreensInfo();
+            }
+        }
+    }
+    else
+    {
+        fprintf(stderr, "Invalid number of arguments\n");
+        fprintf(stderr, "Usage: %s [-info]\n", argv[0]);
+        exit(1);
+    }
 
-    printScreensInfo();
-
-    XCloseDisplay(display);
+    closeScreens();
 
     return 0;
+}
+
+void openScreens()
+{
+    display = XOpenDisplay(NULL);
+    screensInfo = inintScreensInfo();
+}
+
+void closeScreens()
+{
+    if (display != NULL)
+        XCloseDisplay(display);
 }
 
 // returns array
