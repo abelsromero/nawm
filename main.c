500f043842ce27c85eb5dbfbb3a779bf06be2cc5
Abel Salgado Romero <abelromero@gmail.com>
2020-04-13 20:38:42 +0200

    fixes window movement

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
+#include <xdo.h>
 #include <X11/Xlib.h>
 #include <X11/extensions/Xinerama.h>
+#include <X11/extensions/Xrandr.h>
 
 #define SIZE_PIXELS 0
 
@@ -10,31 +13,102 @@ Display *display = NULL;
 XineramaScreenInfo *screensInfo = NULL;
 int screensCount;
 
+void panic(char *message);
+void invalidArguments();
+
 void openScreens();
 void closeScreens();
 
-XineramaScreenInfo *inintScreensInfo();
+XineramaScreenInfo *initScreensInfo();
 void printScreensInfo();
+bool isTargetWindow(unsigned int x, unsigned int y, unsigned int targetScreen);
+
+Window getCurrentWindow(Display *);
+void getWindowProperties(Window w, int *x_ret, int *y_ret, int *width, int *height);
 
 int main(int argc, char **argv)
 {
+    openScreens();
 
-    if (argc == 2)
+    bool completedRun = False;
+
+    xdo_t *xdo;
+    Window win;
+    xdo = xdo_new(NULL);
+    xdo_get_active_window(xdo, &win);
+
+    if (argc > 1)
     {
         for (int i = 1; i < argc; i++)
         {
-            if (strcmp(argv[i], "-info") == 0)
+            if (strcmp(argv[i], "--info") == 0)
             {
                 openScreens();
+                Screen *scr = XScreenOfDisplay(display, 0);
+                printf("dimensions:    %dx%d pixels\n",
+                       XDisplayWidth(display, 0), XDisplayHeight(display, 0));
                 printScreensInfo();
+                completedRun = true;
+            }
+            if (strcmp(argv[i], "--move") == 0)
+            {
+                if (argc > i + 1)
+                {
+                    openScreens();
+
+                    Window window;
+                    int unusedRevertRet;
+                    int ret = XGetInputFocus(display, &window, &unusedRevertRet);
+                    if (ret == 0)
+                        panic("Unexpected xlib error obtaining focus window");
+
+                    unsigned int x, y;
+                    unsigned int win_width, win_height;
+                    getWindowProperties(window, &x, &y, &win_width, &win_height);
+
+                    int targetScreen = atoi(argv[i + 1]);
+                    int originScreen = targetScreen == 0 ? 1 : 0;
+                    if (isTargetWindow(x, y, targetScreen))
+                    {
+                        printf("Ignoring window move: target window is current window");
+                    }
+                    else
+                    {
+                        int targetX, targetY;
+                        // - moving right/left reports +4 in X after moving
+                        // - moving left adds +56 in Y after moving
+                        // targetX = (targetScreen == 1 ? x + screensInfo[0].width : x - screensInfo[0].width) - 4;
+                        if (originScreen == 1)
+                            x -= screensInfo[0].width;
+                        targetX = ((x / (float)screensInfo[originScreen].width) * screensInfo[targetScreen].width) - 4;
+                        if (originScreen == 0)
+                            targetX += screensInfo[0].width;
+
+                        if (targetX < 0)
+                            targetX = 0;
+                        targetY = ((y / (float)screensInfo[originScreen].height) * screensInfo[targetScreen].height) - 56;
+                        if (targetY < 0)
+                            targetY = 0;
+
+                        printf("Moving to\t\t%d, %d", targetX, targetY);
+                        ret = XMoveResizeWindow(display, window, targetX, targetY, win_width, win_height);
+                        if (ret == 0)
+                            panic("Unexpected xlib error moving window");
+                    }
+                    i++;
+                    completedRun = true;
+                }
+                else
+                {
+                    invalidArguments(argv[0]);
+                }
             }
         }
     }
-    else
+
+    if (!completedRun)
     {
-        fprintf(stderr, "Invalid number of arguments\n");
-        fprintf(stderr, "Usage: %s [-info]\n", argv[0]);
-        exit(1);
+        invalidArguments(argv[0]);
     }
 
     closeScreens();
@@ -42,10 +116,23 @@ int main(int argc, char **argv)
     return 0;
 }
 
+void panic(char *message)
+{
+    fprintf(stderr, message);
+    exit(1);
+}
+
+void invalidArguments(char *appName)
+{
+    fprintf(stderr, "Invalid number of arguments\n");
+    fprintf(stderr, "Usage: %s [-info] [-move target_screen_index]\n", appName);
+    exit(1);
+}
+
 void openScreens()
 {
     display = XOpenDisplay(NULL);
-    screensInfo = inintScreensInfo();
+    screensInfo = initScreensInfo();
 }
 
 void closeScreens()
@@ -55,13 +142,12 @@ void closeScreens()
 }
 
 // returns array
-XineramaScreenInfo *inintScreensInfo()
+XineramaScreenInfo *initScreensInfo()
 {
     XineramaScreenInfo *sc = XineramaQueryScreens(display, &screensCount);
     if (sc == NULL)
     {
-        fprintf(stderr, "Xinerama not found");
-        exit(1);
+        panic("Xinerama not found");
     }
     return sc;
 }
@@ -71,6 +157,42 @@ void printScreensInfo()
     printf("Found screens: %d\n", screensCount);
     for (int i = 0; i < screensCount; i++)
     {
-        printf("\t%d: %dx%d pixels\n", screensInfo[i].screen_number, screensInfo[i].width, screensInfo[i].height);
+        printf("\t%d: %dx%d\n", screensInfo[i].screen_number, screensInfo[i].width, screensInfo[i].height);
     }
+}
+
+bool isTargetWindow(unsigned int x, unsigned int y, unsigned int targetScreen)
+{
+    if (targetScreen == 0 && x < screensInfo[0].width)
+        return true;
+
+    if (targetScreen == 1 && x > screensInfo[0].width)
+        return true;
+
+    return false;
+}
+
+Window getCurrentWindow(Display *d)
+{
+    Window focused;
+    int revert_to;
+    XGetInputFocus(display, &focused, &revert_to);
+    return focused;
+}
+
+void getWindowProperties(Window w, int *x_ret, int *y_ret, int *width, int *height)
+{
+    XWindowAttributes attr;
+    int ret = XGetWindowAttributes(display, w, &attr);
+    if (ret == 0)
+        panic("Unexpected xlib error obtaining window attributes");
+
+    int translated_x, translated_y;
+    Window unused_child;
+    XTranslateCoordinates(display, w, attr.root, attr.x, attr.y, &translated_x, &translated_y, &unused_child);
+    printf("Window properties: x=%d,y=%d; w=%d,h=%d\n", translated_x, translated_y, attr.width, attr.height);
+    *x_ret = translated_x;
+    *y_ret = translated_y;
+    *width = attr.width;
+    *height = attr.height;
 }
